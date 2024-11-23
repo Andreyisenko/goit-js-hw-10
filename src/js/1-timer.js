d1b576dd4406c75e4f73b1ba3edcbb8c1b6b9324
Andrey Isenko <aisenko87@gmail.com>
2024-11-23 20:51:18 +0200

    add task-1

---
+++ src/js/1-timer.js
@@ -0,0 +1,7 @@
+console.log("Timer");
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const inpT = document.querySelector("#datetime-picker")
+console.log(inpT);
+flatpickr(inpT, {});
