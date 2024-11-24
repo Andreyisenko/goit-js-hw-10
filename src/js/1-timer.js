55a8e888670f0f9c607766ca307dae8f36eaf19e
Andrey Isenko <aisenko87@gmail.com>
2024-11-24 16:31:52 +0200

    task-1 work

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,7 +1,26 @@
-console.log("Timer");
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+// console.log("Timer");
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 
-const inpT = document.querySelector("#datetime-picker")
-console.log(inpT);
-flatpickr(inpT, {});
+const inpT = document.querySelector('#datetime-picker');
+// console.log(inpT);
+let userSelectedDate;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    userSelectedDate = selectedDates[0];
+    console.log(userSelectedDate);
+  },
+};
+
+flatpickr(inpT, options);
+
+// inpT.addEventListener('input', handleInput);
+
+// function handleInput(event) {
+//   // console.log(event.target.value);
+// }
