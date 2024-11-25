39b5a16431020186694bd8afddd2c4558cd78584
Andreyisenko <aisenko87@gmail.com>
2024-11-25 07:06:48 +0000

    Deploying to gh-pages from @ Andreyisenko/goit-js-hw-10@9570e905cccfcb6120dc2deafad11a56ed3f5ebe 🚀

--- assets/1-timer-Bvtxz2cs.js
+++
@@ -1,2 +0,0 @@
-import{f as o}from"./vendor-EyZmBGcZ.js";const n=document.querySelector("#datetime-picker");let t;const r={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0]),t=e[0],console.log(t)}};o(n,r);
-//# sourceMappingURL=1-timer-Bvtxz2cs.js.map
