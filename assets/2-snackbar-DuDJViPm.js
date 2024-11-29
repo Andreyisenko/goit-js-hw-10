0c1c173cd0b812d50049f3a638666272dbfd14be
Andreyisenko <aisenko87@gmail.com>
2024-11-29 06:05:33 +0000

    Deploying to gh-pages from @ Andreyisenko/goit-js-hw-10@d72f1d9a42ee704af0bbae4a6e14d42b2e9a11f6 🚀

--- assets/2-snackbar-DuDJViPm.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("button");const e=document.querySelector(".form");e.addEventListener("submit",t);function t(o){console.log(o),console.log("ok")}
-//# sourceMappingURL=2-snackbar-DuDJViPm.js.map
